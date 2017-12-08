3f8913a2d561ce412a6a0aac2324791c9c7dfdd3
Kumar <abhishek.ch.kumar@accenture.com>
2017-12-08 16:02:51 +0530

    added .env file

--- dukebotpoc/app.js
+++ dukebotpoc/app.js
@@ -1,10 +1,12 @@
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
 var restify = require('restify');
 var builder = require('botbuilder');
-var cognitiveservices = require('../../lib/botbuilder-cognitiveservices');
+//var cognitiveservices = require('../../lib/botbuilder-cognitiveservices');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 3980, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
@@ -17,19 +19,31 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-/*// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
 	if(session.message.text=="hi")
 		session.send("Helloo");
 	else
     session.send("You said: %s", session.message.text);
-});*/
+});
+
+//<Recognizer> 
+// Add global LUIS recognizer to bot 
+ /*var luisAppUrl = process.env.LUIS_APP_URL || 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/8c7a3abe-a998-4709-a9d4-bfa2793f3ef9?subscription-key=debec9e5cba24fc28656f7eae27df019'; 
+ var notesRecognizer = bot.recognizer(new builder.LuisRecognizer(luisAppUrl)); */
+
+//You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
+//This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+ // </Recognizer> 
+
 
 //=========================================================
 //Recognizers
 //=========================================================
 
-var qnarecognizer = new cognitiveservices.QnAMakerRecognizer({
+/*var qnarecognizer = new cognitiveservices.QnAMakerRecognizer({
 	knowledgeBaseId: '3cecefd4-032e-4024-be3a-70282d15d61b', 
 	subscriptionKey: '14f2e53d7e084af6a5d8a70cc1058e37',
  top: 4});
@@ -58,5 +72,5 @@ intents.onDefault([
  function(session){
      session.send('Sorry!! No match!!');
 	}
-]);
+]);*/
 
